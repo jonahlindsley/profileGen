bec318c446905f21ebaf480a6e73d2ad6d93c73f
jonah lindsley <jonahlindsley@yahoo.com>
2022-07-21 19:13:28 -0500

    added files for each class

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 
 const inquirer = require('inquirer')
-// const require()
+const fs = require('fs')
+
+
+const questions = [
+{
+name: 'userName',
+message: "enter team member's name"
+},
+{
+    name: 'id',
+    message: "enter team member's id number"
+    },
+    {
+        name: 'email',
+        message: "enter team member's email address"
+        }
+]
 
 
 
 
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        console.log(data),
+        err ? console.log(err) : console.log('README.md has been made');
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Vehicle;
