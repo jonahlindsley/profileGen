1b846a6792641afef4d10b548875133d91d33d09
jonah lindsley <jonahlindsley@yahoo.com>
2022-08-15 18:21:10 -0500

    properly creating website

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 
 const inquirer = require('inquirer')
-const fs = require('fs');
+const generateHtml = require('./src/teamGenerator')
+const fs = require('fs');   
 const Engineer = require('./lib/Engineer');
 const Manager = require('./lib/Manager');
 const Intern = require('./lib/Intern');
 let teamMembers = []
-const managerQuestions = () => {
-    return inquirer.prompt([
+const managerQuestions = async () => {
+    const managerQuestions = await inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: "enter team member's name",
+            message: "enter managers's name",
         },
         {
             type: 'input',
             name: 'id',
-            message: "enter team member's id number",
+            message: "enter managers's id number",
         },
         {
             type: 'input',
             name: 'email',
-            message: "enter team member's email address",
+            message: "enter managers's email address",
         },
         {
             type: 'input',
             name: 'officeNumber',
-            message: 'enter managers office number',
+            message: "enter managers's office number",
         }
-    ]).then(managerQuestions => {
-        const { name, id, email, officeNumber } = managerQuestions;
-        const manager = new Manager(name, id, email, officeNumber)
-        teamMembers.push(manager)
-        console.log(manager)
-    })
+    ]);
+    const { name, id, email, officeNumber } = managerQuestions;
+    const manager = new Manager(name, id, email, officeNumber);
+    teamMembers.push(manager);
+    console.log(manager.getRole());
 };
 
 //engineer questions
 
-const employeeQuestions = () => {
-    return inquirer.prompt(
+const employeeQuestions = async () => {
+    const employeeQuestions = await inquirer.prompt(
         [{
             type: 'list',
             name: 'role',
-            message: "choose a new role",
+            message: "add the next role",
             choices: ['engineer', 'intern']
         },
         {
@@ -61,69 +61,68 @@ const employeeQuestions = () => {
             message: "enter employee's email address",
         },
         {
-            when: (input) => input.role === 'engineer',
+            when: (input_1) => input_1.role === 'engineer',
             type: 'input',
-            name: 'gitHub',
+            name: 'github',
             message: "enter engineer's github",
         },
         {
-            when: (input) => input.role === 'intern',
+            when: (input_3) => input_3.role === 'intern',
             type: 'input',
             name: 'school',
             message: "enter interns's school",
         },
-        {
-            type: 'confirm',
-            name: 'addAnother',
-            message: 'would you like to add another team member?',
-            default: 'false'
-        }
-        ])
-        .then(employeeQuestions => {
-            let { name, id, email, role, github, school, addAnother } = employeeQuestions;
-            let employee;
-            if (role === 'engineer') {
-                employee = new Engineer(name, id, email, github)
-                console.log(employee)
-            } else if (role === 'intern') {
-                employee = new Intern(name, id, email, school)
-                console.log(employee)
-            }
-            teamMembers.push(employee);
-            if (addAnother) {
-                return employeeQuestions(teamMembers);
-            } else {
-                return teamMembers;
-            }
-        })
+            ])
+    let { name, id, email, role, github, school} = employeeQuestions;
+    let employee;
+    if (role === 'engineer') {
+        employee = new Engineer(name, id, email, github);
+        teamMembers.push(employee);
+        console.log(employee)
+        return teamMembers;
+    } else if (role === 'intern') {
+        employee = new Intern(name, id, email, school);
+        teamMembers.push(employee);
+        console.log(employee)
+        return teamMembers;
+    }
+
 }
 
-
-
-
-
+const addAnother = async () => {
+    const addAnother = await inquirer.prompt(
+[{
+    type: 'confirm',
+    name: 'addAnother',
+    message: 'would you like to add another team member?',
+    default: 'false'
+}])
+if (addAnother === true) {
+    return employeeQuestions();
+} 
+}
 
 const init = (data) => {
     fs.writeFile('./dist/index.html', data, err => {
-        console.log(data)
         err ? console.log(err) : console.log('new team created')
     })
 }
 
-
+startTeam = () => {
 managerQuestions()
+.then(employeeQuestions)
+.then(addAnother)
 .then(
-    employeeQuestions().then(teamMembers => {
-        return teamMembers;
-    })
-);
-
-
-// managerQuestions()
-// .then(employeeQuestions()).then(teamMembers => {
-//     return generateHtml(teamMembers)
-// }).then(pageHtml => {
-//     return init(pageHtml)
-// }).catch(err => {
-//     console.log(err)
-// })
+    teamMembers => {
+    console.log('these are the teamMembers')
+    console.log(teamMembers)
+    return generateHtml(teamMembers)
+}).then(pageHtml => {
+    console.log('this is the page html below ')
+    console.log(pageHtml)
+    return init(pageHtml)
+}).catch(err => {
+    console.log(err)
+})
+}
+startTeam()
--- src/teamGenerator.js
+++ src/teamGenerator.js
@@ -1,121 +1,130 @@
 
+
 // manager card
 const managerCard = (manager) => {
     return `  <div class="row">
-<div class="col s12 m6">
-  <div class="card blue-grey darken-1">
-    <div class="card-content white-text">
-      <ul class="collection with-header">
-      <li class="collection-header"><h4>${manager.name}</h4></li>
-      <li class="collection-header"><h4>${manager.role}</h4></li>
-      <li class="collection-item">id ${manager.id}</li>
-      <li class="collection-item">email ${manager.email}</li>
-      <li class="collection-item">office number ${manager.officeNumber}</li>
-    </ul>
+    <div class="col s8 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">${manager.name}</span>
+          <p>id: ${manager.id}</p>
+          <p>office number: ${manager.officeNumber}</p>
+        </div>
+        <div class="card-action">
+        <p>email:<a href="mailto:${manager.email}">${manager.email}</a></p>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>`}
+  </div>`}
 
+                
 
 // engineer card
 const engineerCard = (engineer) => {
-    return `  <div class="row">
-<div class="col s12 m6">
-  <div class="card blue-grey darken-1">
-    <div class="card-content white-text">
-      <ul class="collection with-header">
-      <li class="collection-header"><h4>$engineer.name}</h4></li>
-      <li class="collection-header"><h4>${engineer.role}</h4></li>
-      <li class="collection-item">id ${engineer.id}</li>
-      <li class="collection-item">email ${engineer.email}</li>
-      <li class="collection-item"><a href="https://github.com/${engineer.github}">my github</a></li>
-    </ul>
-    </div>
-  </div>
-</div>
-</div>`
+    return `  
+      <div class="col s6 m4">
+        <div class="card blue-grey darken-1">
+          <div class="card-content white-text">
+            <span class="card-title">${engineer.name}</span>
+            <p>${engineer.role}</p>
+            <p>id: ${engineer.id}</p>
+          </div>
+          <div class="card-action">
+          <p>email:<a href="mailto:${engineer.email}">${engineer.email}</a></p>
+          <a href="https://github.com/${engineer.github}">my github</a>
+          </div>
+        </div>
+      </div>`
 }
 
+        
 // intern card
 const internCard = (intern) => {
-    return `<div class="row">
-<div class="col s12 m6">
-  <div class="card blue-grey darken-1">
-    <div class="card-content white-text">
-      <ul class="collection with-header">
-      <li class="collection-header"><h4>${intern.name}</h4></li>
-      <li class="collection-header"><h4>${intern.role}</h4></li>
-      <li class="collection-item">id ${intern.id}</li>
-      <li class="collection-item">email ${intern.email}</li>
-      <li class="collection-item">school ${intern.school}</li>
-    </ul>
-    </div>
-  </div>
-</div>
-</div>`
+    return ` <div class="col s6 m4">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">${intern.name}</span>
+          <p>${intern.role}</p>
+          <p>id: ${intern.id}</p>
+          <p>school: ${intern.school}</p>
+        </div>
+        <div class="card-action">
+        <p>email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+        </div>
+      </div>
+    </div>`
 }
 
-const generateHTML = (data) => {
-    employeesArray = []
-    for (let i = 0; i < data.length; i++) {
-        const employee = data[i];
+const generateHTML = (answers) => {
+  let managerArray = []
+  let employeesArray = []
+    for (let i = 0; i < answers.length; i++) {
+        const employee = answers[i];
         const role = employee.getRole();
-        if (role === manager){
+        if (role === 'Manager'){
             const managersCard = managerCard(employee)
-            employeesArray.push(managersCard)
+            managerArray.push(managersCard)
         }
 
-        if (role === engineer){
-            const engineerCard = engineerCard(employee)
+        if (role === 'Engineer'){
+            const engineersCard = engineerCard(employee)
             employeesArray.push(engineersCard)
         }
 
-        if (role === intern){
+        if (role === 'Intern'){
             const internsCard = internCard(employee)
             employeesArray.push(internsCard)
         }
 
-        const employeeCards = employeesArray.join('')
-        const generateEmployeesPage = generatePage(employeeCards)
+    }
+  
+ 
+    const employeeCards = employeesArray.join('')
+    
+    const generatePage = (employeesCards) => {
+      let newPage = `<!DOCTYPE html>
+       <html lang="en">
+       <head>
+           <meta charset="UTF-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <!-- Compiled and minified CSS -->
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+           <link rel="stylesheet" href="./assets/style.css">
+           <title>our team</title>
+       </head>
+       <body>
+           <nav>
+               <div class="nav-wrapper">
+                 <a href="#" class="brand-logo center">Logo</a>
+                 <ul id="nav-mobile" class="left hide-on-med-and-down">
+                 </ul>
+               </div>
+             </nav>
+             <div class="container">
+             ${managerArray}
+             <div class="row">
+              ${employeesCards}
+              </div>
+             </div>
+         <!-- Compiled and minified JavaScript -->
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+       </body>
+       </html>`
+       return newPage
     }
 
+    const generateEmployeesPage = generatePage(employeeCards)
 
-const generatePage = (employeesCards) => {
-   return  `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./assets/style.css">
-        <title>our team</title>
-    </head>
-    <body>
-        <nav>
-            <div class="nav-wrapper">
-              <a href="#" class="brand-logo center">Logo</a>
-              <ul id="nav-mobile" class="left hide-on-med-and-down">
-              </ul>
-            </div>
-          </nav>
+return generateEmployeesPage
+  }
     
-          <body>
-          <div class="container">
-           ${employeesCards}
-          </div>
-        </body>
-    
-    
-    
-      <!-- Compiled and minified CSS -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    
-      <!-- Compiled and minified JavaScript -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-              
-    </body>
-    </html>`
-}
+function init(answers) {
+
+return generateHTML(answers)
 
 }
+
+
+
+module.exports = init;
